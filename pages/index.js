19e076a7f422836690ebf10c634e0769c8d8e09d
Adam Sobotka <vorcigernix@gmail.com>
2020-05-21 23:43:59 +0200

    post type filter

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import Container from '../components/container'
 import MoreStories from '../components/more-stories'
 import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
 import Navigation from '../components/navigation'
 import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
 import Head from 'next/head'
 
 export default function Index({ preview, allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  const articles = allPosts.filter(post=> !post.type.includes('Video'))
+  const heroPost = articles[0]
+  const morePosts = allPosts.filter(item => item !== heroPost)
   return (
     <>
       <Layout preview={preview}>
