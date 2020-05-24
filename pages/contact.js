a947eb52957cce6ae85dd8d0014d56a9b9d1a026
Adam Sobotka <vorcigernix@gmail.com>
2020-05-24 17:59:46 +0200

    label fix

--- pages/contact.js
+++ pages/contact.js
@@ -82,7 +82,7 @@ export default function Contact() {
               <div className="md:w-1/3">
                 <label
                   className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                  for="inline-full-name"
+                  htmlFor="inline-full-name"
                 >
                   Full Name
                 </label>
