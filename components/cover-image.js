c8c2924de9b34637af31d8d3fff0c5219bdb33a4
Adam Sobotka <vorcigernix@gmail.com>
2020-05-27 13:31:55 +0200

    cover image tuneup

--- components/cover-image.js
+++ components/cover-image.js
@@ -1,12 +1,21 @@
 import Link from "next/link";
 
-export default function CoverImage({url, slug }) {
-  const bgPicture = {
-    background: `url(${url}?w=600&fm=webp&q=80) no-repeat center/cover`,
-  };
+export default function CoverImage({ url, slug }) {
   return (
-    <Link as={`/posts/${slug}`} href="/posts/[slug]">
-      <div className="-mx-5 sm:mx-0 h-64" style={bgPicture} />
-    </Link>
+    <div className="h-64 overflow-hidden ">
+      <Link as={`/posts/${slug}`} href="/posts/[slug]">
+        <picture>
+          <source media="(min-width: 800px)" srcSet={`${url}?w=800&fm=webp&q=80`} />
+          <source media="(min-width: 800px)" srcSet={`${url}?w=800&fm=jpg&q=90`} />
+          <source media="(max-width: 799px)" srcSet={`${url}?w=400&fm=webp&q=80`} />
+          <source media="(max-width: 799px)" srcSet={`${url}?w=400&fm=jpg&q=90`} />
+          <img
+            src={url}
+            alt={`Cover Image`}
+            className="w-auto"
+          />
+        </picture>
+      </Link>
+    </div>
   );
 }
