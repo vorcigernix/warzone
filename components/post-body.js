a092d08af29b17d56d8444e43ece163483245071
Adam Sobotka <vorcigernix@gmail.com>
2020-06-16 19:26:21 +0200

    handle inline images

--- components/post-body.js
+++ components/post-body.js
@@ -1,12 +1,28 @@
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
-import markdownStyles from './markdown-styles.module.css'
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BLOCKS } from "@contentful/rich-text-types";
+import markdownStyles from "./markdown-styles.module.css";
+
+const options = {
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: (node) => {
+      console.log(node.data.target.fields.title)
+      return (
+        <img
+          title={node.data.target.fields.title}
+          src={node.data.target.fields.file.url}
+        />
+      );
+    },
+  },
+};
 
 export default function PostBody({ content }) {
+  //console.log(content);
   return (
-    <div className="mx-auto text-white mb-8">
-      <div className={markdownStyles['markdown']}>
-        {documentToReactComponents(content)}
+    <div className="max-w-2xl mx-auto">
+      <div className={markdownStyles["markdown"]}>
+        {documentToReactComponents(content,options)}
       </div>
     </div>
-  )
+  );
 }
